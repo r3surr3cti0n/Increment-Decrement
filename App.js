1ac7ac6c47dbeace3d07d34bad9332ce09330895
r3surr3cti0n <r3surr3cti0n@protonmail.com>
2021-12-01 19:40:38 +0100

    all

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [szam, setSzam] = useState(0);
+
+	const increment = () => {
+		setSzam(szam + 1);
+	};
+	const decrement = () => {
+		setSzam(szam - 1);
+	};
+
+	const reset = () => {
+		setSzam(0);
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text style={{ fontSize: 23 }}>{szam}</Text>
+
+			<View style={styles.flex}>
+				<TouchableOpacity style={styles.btn} onPress={decrement}>
+					<Text>decrement</Text>
+				</TouchableOpacity>
+
+				<TouchableOpacity style={styles.btn} onPress={increment}>
+					<Text>increment</Text>
+				</TouchableOpacity>
+			</View>
+
+			<TouchableOpacity style={styles.btn} onPress={reset}>
+				<Text>reset</Text>
+			</TouchableOpacity>
+
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	btn: {
+		display: "block",
+		fontSize: 20,
+		color: "#000",
+		margin: 10,
+		borderRadius: 5,
+		border: "1px solid black",
+		padding: "5px",
+	},
+	flex: {
+		flexDirection: "row",
+	},
 });
